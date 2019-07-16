fe6e9b50dc087030b6df677ab90751fdde703e6b
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-16 07:32:56 -0300

    Desafio05 - Paginação e filtro

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -101,3 +101,52 @@ export const IssueList = styled.ul`
     }
   }
 `;
+
+export const IssueFilter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 30px 30px 30px;
+
+  button {
+    border-radius: 4px;
+    outline: 0;
+    border: 0;
+    padding: 10px;
+
+    &:nth-child(${props => props.active + 1}) {
+      background: #7159c1;
+      color: #fff;
+    }
+
+    &:hover {
+      background: #7159c1;
+      color: #fff;
+    }
+  }
+`;
+
+export const PageActions = styled.div`
+  padding-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+
+  button {
+    transition: opacity 0.25s ease-out;
+    border-radius: 4px;
+    outline: 0;
+    border: 0;
+    padding: 10px;
+
+    &:disabled {
+      opacity: 0.35;
+      cursor: not-allowed;
+    }
+
+    &:hover {
+      background: #7159c1;
+      color: #fff;
+    }
+  }
+`;
